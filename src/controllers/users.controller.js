cc323977fe2a68d65451338f79a7f2e983a568ff
julia martins <juliamartinss.564@gmail.com>
2024-04-17 14:58:44 -0300

    feat: tratação de erros

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -6,16 +6,23 @@ import UsersRepository from "../models/users/UsersRepository.js";
 const usersRepository = new UsersRepository();
 
 export const getUsers = async (req, res) => {
-  const users = await usersRepository.getUsers();
+  try {
+    const users = await usersRepository.getUsers();
 
   if (!users) {
     return res.status(404).send({ message: "Não há usuários cadastrados" });
   }
   return res.status(200).send({ totalUsers: users.length, users });
+  } catch(error){
+    return res
+    .status(500)
+    .send({ message: "Erro ao buscar usuarios", error: error.message });
+  }
 };
 
 export const getUserById = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
   const user = await usersRepository.getUserById(id);
 
@@ -24,10 +31,16 @@ export const getUserById = async (req, res) => {
   }
 
   return res.status(200).send({ message: "Usuário encontrado", user });
+  } catch(error){
+    return res
+    .status(500)
+    .send({ message: "Erro ao buscar usuario por id", error: error.message });
+}
 };
 
 export const createUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
   const userAlreadyExists = await usersRepository.getUserByEmail(email);
 
@@ -42,10 +55,16 @@ export const createUser = async (req, res) => {
   await usersRepository.createUser(user);
 
   return res.status(201).send({ message: "Usuário criado com sucesso", user });
+  } catch(error){
+    return res
+    .status(500)
+    .send({ message: "Erro ao criar usuario", error: error.message });
+}
 };
 
 export const updateUser = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
   const { name, email, password } = req.body;
 
   const userById = await usersRepository.getUserById(id);
@@ -66,10 +85,16 @@ export const updateUser = async (req, res) => {
   return res
     .status(200)
     .send({ message: "Usuário atualizado com sucesso", user });
+  } catch(error){
+    return res
+    .status(500)
+    .send({ message: `Erro ao atualizar usuario com ${id}`, error: error.message });
+}
 };
 
 export const deleteUser = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
   const user = await usersRepository.getUserById(id);
 
@@ -82,4 +107,9 @@ export const deleteUser = async (req, res) => {
   return res
     .status(200)
     .send({ message: "Usuário deletado com sucesso", user });
+  } catch(error){
+    return res
+    .status(500)
+    .send({ message: `Erro ao deletar usuario com id ${id}`, error: error.message });
+}
 };
--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -6,38 +6,59 @@ export default class UsersRepository {
   }
 
   async getUsers() {
-    const allUsers = await this.pg.manyOrNone("SELECT * FROM users");
-    // console.log(allUsers);
-    return allUsers;
+    try {
+      const allUsers = await this.pg.manyOrNone("SELECT * FROM users");
+      // console.log(allUsers);
+      return allUsers;
+    } catch (error) {
+      // console.log("failed to get all users", error);
+      throw error;
+    }
   }
 
   async getUserById(id) {
-    const user = await this.pg.oneOrNone(
-      "SELECT * FROM users WHERE id = $1",
-      id
-    );
-    console.log(user);
-    return user;
+    try {
+      const user = await this.pg.oneOrNone(
+        "SELECT * FROM user WHERE id = $1",
+        id
+      );
+      // console.log(user);
+      return user;
+    } catch (error) {
+      // console.log(`Failed to get userss with id ${id}`, error);
+      throw error;
+    }
   }
 
   async getUserByEmail(email) {
+   try {
     const user = await this.pg.oneOrNone(
       "SELECT * FROM users WHERE email = $1",
       email
     );
     return user;
+   } catch (error) {
+    // console.log("failed to get all users by email", error);
+    throw error;
   }
+}
 
   async createUser(user) {
-    await this.pg.none(
-      "INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)",
-      [user.id, user.name, user.email, user.password]
-    );
-    return user;
+    try {
+      await this.pg.none(
+        "INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)",
+        [user.id, user.name, user.email, user.password]
+      );
+      return user;
+    } catch (error) {
+      // console.log("failed to get all users create", error);
+      throw error;
   }
+}
 
   async updateUser(id, name, email, password) {
-    const user = await this.getUserById(id);
+    try {
+      const user = await this.getUserById(id);
 
     if (!user) {
       return null;
@@ -47,9 +68,18 @@ export default class UsersRepository {
       [name, email, password, id]
     );
     return updateUser;
+    } catch (error) {
+      // console.log(`Failed to get user with id ${id}`, error);
+      throw error;
   }
+}
 
   async deleteUser(id) {
-    await this.pg.none("DELETE FROM users WHERE id = $1", id);
+    try {
+      await this.pg.none("DELETE FROM users WHERE id = $1", id);
+    } catch (error) {
+      // console.log(`Failed to get user with id ${id}`, error);
+      throw error;
   }
+}
 }
